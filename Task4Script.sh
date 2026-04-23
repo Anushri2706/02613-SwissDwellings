82d441f8f42a37ce7caa01f26bc678b4dddcdad7
Anushri2706 <anushri.gupta@maersk.com>
2026-04-23 17:34:55 +0200

    task 4

--- Task4Script.sh
+++ Task4Script.sh
@@ -8,7 +8,6 @@
 #BSUB -o batch_output/mini_project_%J.out
 #BSUB -e batch_output/mini_project_%J.err
 
-
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613
 kernprof -l -v simulate.py
